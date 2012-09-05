35a824b7ccbd0ace13b4adf91cb3d131dd1c7dfc
Vyacheslav Enis <venis@difane.com>
2012-09-05 20:14:09 +0300

    Created initial dummy test suite.

---
+++ Tests/Entity/ContentPartTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Difane\Bundle\ContentPartBundle\Tests\Entity;
+
+class ContentPartTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
